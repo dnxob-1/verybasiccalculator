dab83cdd0b7cb27f5c8e551745fc92018898d8b0
morgan <ff6.6.ffdnxob.pthd.6@gmail.com>
2024-11-10 20:34:42 -0600

    added a prototype, probably wont finish

--- tryingSomething.py
+++ tryingSomething.py
@@ -1,15 +1,39 @@
 import math
+import re
+
+global x
+global y
+global z
+z = 0
 
 print("Enter a calc: ")
-x = str(input(""))
+y = str(input(""))
 
-
-if "+" in x:
-    print(True)
-    y = x.split()  # split on + so you can find the ints and then add or such
-    print(y)
-    for i in y:
-        if isinstance(i, int):
-            print("There is a num")
-else:
-    print(False)
+for i in range(len(y)):
+    try:
+        if "+" in y:
+            y = [int(num.strip()) for num in re.split(r"\+", y)]
+            print("sum : ", sum(y))
+        elif "-" in y:
+            y = [int(num) for num in re.findall(r"[+-]?\d+", y)]
+            print("sum : ", sum(y))
+        elif "*" in y:
+            y = [int(num.strip()) for num in re.split(r"\*", y)]
+            # y = [int(item.strip()) for item in y]
+            # print(y)
+            # y[i] = int(y[i - 1]) * int(y[i + 1])
+            # y.remove(y[i + 1])
+            prod = y[0]
+            for n in y[1:]:
+                prod *= n
+            print(y)
+            print("sum : ", sum(y))
+        elif "sin" in y:
+            print("there is a sin")
+            # y = [s.replace(f"sin({z})", "") for s in y]
+            print(y)
+            # assign the float to a var and then change to str for replace
+            # y = [s.replace("sin()", math.sin(x)) for s in y]
+    except ValueError as err:
+        print(y)
+        print(f"Invalid numbers: {err}")
